882a3e60e7d30621333999ced19bd121eedc2214
stefenSitorus <antonius.stefen@gmail.com>
2018-10-09 16:36:09 +0700

    first commit

---
+++ app/src/main/java/com/example/mra/newsmotogp/NewsMotoGp.java
@@ -0,0 +1,26 @@
+package com.example.mra.newsmotogp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class NewsMotoGp extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_moto_gp);
+    }
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, berita1.class);
+        EditText editText = (EditText) findViewById(R.id.textView);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
